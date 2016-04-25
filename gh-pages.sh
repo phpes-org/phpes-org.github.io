b9c7ba4500f4cbeacc94236bd821f4dc199c0b10
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2016-04-25 14:43:26 -0300

    site update

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-sculpin generate --env=prod
+git clone http://github.com/phpes-org/phpes-org.github.io.git output_prod
 cd output_prod/
-git init
-git remote add origin git@github.com:phpes-org/phpes-org.github.io.git
+git pull origin master
+cd ../
+sculpin generate --env=prod
 git add -A
 git commit -m 'site update'
 git push origin master
