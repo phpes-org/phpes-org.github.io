5a5ebec705f3cc186c5bca3477172ea2e77da362
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2016-05-03 20:02:23 -0300

    pull after push

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-git clone http://github.com/phpes-org/phpes-org.github.io.git output_prod
+git clone --depth=1 http://github.com/phpes-org/phpes-org.github.io.git output_prod
 cd output_prod/
 git pull origin master
 cd ../
 sculpin generate --env=prod
+cd output_prod/
 git add -A
 git commit -m 'site update'
 git push origin master
