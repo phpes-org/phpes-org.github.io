515d3fe13cf11754b65585d9d4b438d1592977cf
Rogerio Lino <rogeriolino@gmail.com>
2024-11-14 15:07:25 -0300

    feat: upgrade sculpin

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+rm -rf output_prod/
 git clone --depth=1 https://github.com/phpes-org/phpes-org.github.io.git output_prod
 cd output_prod/
 git pull origin master
 cd ../
-sculpin generate --env=prod
+vendor/bin/sculpin generate --env=prod
 cd output_prod/
 git add -A
 git commit -m 'site update'
